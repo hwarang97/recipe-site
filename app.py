52fc9ce5fee1ccfa4346f3259e8908265a87e6c3
dolggul2 <dolggul2@gmail.com>
2026-03-05 22:03:48 +0900

    feat: temp

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from pymongo import MongoClient
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import jsonify
+from flask import make_response
+from pymongo import MongoClient
+
+import hmac
+import hashlib
+import base64
+
+
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
-db = client.db_food
+
+client = MongoClient("localhost", 27017)
+db = client.jungle
+
+load_dotenv()
+secret = os.getenv("SECRET")
+
 
 @app.route("/", methods=["GET"])
 def home():
@@ -13,51 +29,63 @@ def home():
 
 @app.route("/recipe", methods=["GET"])
 def recipe():
-    # return render_template("recipe.html")
-    food = db.recipe.find_one({"title": "김치볶음밥"})
-    title = food["title"]
-    content = food["content"]
-    logo_location = "static/logo.png"
-    image_location  = food["image"]
-    return render_template("recipe.html", title=title, content=content, logo_location=logo_location, image_location=image_location) # "static/logo.jpg"
+    return render_template("recipe.html")
+    # TODO: DB에서 데이터를 가져와, 파라미터로 넘겨주면 됩니다. DB 만들고 나서 주석부분 진행해보세요.
+    # return render_template("recipe.html", title=title, content=content, logo_location=logo_location, image_location=image_location)
 
 
 @app.route("/login", methods=["GET"])
 def login():
-    # return render_template("login.html")
+    return render_template("login.html")
     # TODO
-    food = db.recipe.find_one({"title": "김치볶음밥"})
-    logo_location = "static/logo.png"
-    return render_template("login.html", logo_location=logo_location)
+    # return return render_template("login.html", logo_location=logo_location)
+
+
+@app.route("/login", methods=["POST"])
+def authenticate():
+    id = request.form.get("id")
+    password = request.form.get("password")
+    stored_user = db.users.find_one({"id": f"{id}"})
+    if stored_user and stored_user["password"] == password:
+        response = make_response(jsonify({"result": "success", "id": f"{id}"}))
+
+        # 모듈에 저장해서 불러오기
+        # header = b'{"alg": "HS256", "typ": "JWT"}'
+        # payload = b'{"iss": "whatdoieattoday", "db_id": "ksh"}'
+        # encoded_header = base64.b64encode(header)
+        # encoded_payload = base64.b64encode(payload)
+        # message = f"{encoded_header}.{encoded_payload}".encode("utf-8")
+        # signature = hmac.new(secret.encode("utf-8"), message, hashlib.sha256)
+        # jwt = f"{encoded_header}.{encoded_payload}.{signature}"
+        # response.set_cookie("jwt", jwt, httponly=True)
+
+        # print(response.headers)
+
+        # return response
+        jsonify({"result": "success"})
+    else:
+        return jsonify({"result": "fail"})
 
 
 @app.route("/mypage", methods=["GET"])
 def mypage():
-    #return render_template("mypage.html")
+    return render_template("mypage.html")
     # TODO
-    food = db.recipe.find_one({"title": "김치볶음밥"})
-    logo_location = "static/logo.png"
-    content = food["content"]
-    name = food["id"]
-    return render_template("mypage.html", logo_location=logo_location, name=name, content=content)
+    # return render_template("mypage.html", logo_location=logo_location, name=name, contents=contents)
 
 
 @app.route("/find", methods=["GET"])
 def find():
-    #return render_template("find.html")
+    return render_template("find.html")
     # TODO
-    food = db.recipe.find_one({"title": "김치볶음밥"})
-    logo_location = "static/logo.png"
-    return render_template("find.html", logo_location=logo_location)
+    # return render_template("find.html", logo_location=logo_location)
 
 
 @app.route("/signin", methods=["GET"])
 def signin():
-    #return render_template("signin.html")
+    return render_template("signin.html")
     # TODO
-    food = db.recipe.find_one({"title": "김치볶음밥"})
-    logo_location = "static/logo.png"
-    return render_template("signin.html", logo_location=logo_location)
+    # return render_template("signin.html", logo_location=logo_location)
 
 
 if __name__ == "__main__":

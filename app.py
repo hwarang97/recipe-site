faa46a624eb4963f48a987f4dcf00c76fd121ee3
Hyeon <seunghyeon8721@gmail.com>
2026-03-05 18:08:48 +0900

    feat: retrive data from db

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from pymongo import MongoClient
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+client = MongoClient('localhost', 27017)
+db = client.db_food
 
 @app.route("/", methods=["GET"])
 def home():
@@ -11,37 +13,51 @@ def home():
 
 @app.route("/recipe", methods=["GET"])
 def recipe():
-    return render_template("recipe.html")
-    # TODO: DB에서 데이터를 가져와, 파라미터로 넘겨주면 됩니다. DB 만들고 나서 주석부분 진행해보세요.
-    # return render_template("recipe.html", title=title, content=content, logo_location=logo_location, image_location=image_location)
+    # return render_template("recipe.html")
+    food = db.recipe.find_one({"title": "김치볶음밥"})
+    title = food["title"]
+    content = food["content"]
+    logo_location = "static/logo.png"
+    image_location  = food["image"]
+    return render_template("recipe.html", title=title, content=content, logo_location=logo_location, image_location=image_location) # "static/logo.jpg"
 
 
 @app.route("/login", methods=["GET"])
 def login():
-    return render_template("login.html")
+    # return render_template("login.html")
     # TODO
-    # return return render_template("login.html", logo_location=logo_location)
+    food = db.recipe.find_one({"title": "김치볶음밥"})
+    logo_location = "static/logo.png"
+    return render_template("login.html", logo_location=logo_location)
 
 
 @app.route("/mypage", methods=["GET"])
 def mypage():
-    return render_template("mypage.html")
+    #return render_template("mypage.html")
     # TODO
-    # return render_template("mypage.html", logo_location=logo_location, name=name, contents=contents)
+    food = db.recipe.find_one({"title": "김치볶음밥"})
+    logo_location = "static/logo.png"
+    content = food["content"]
+    name = food["id"]
+    return render_template("mypage.html", logo_location=logo_location, name=name, content=content)
 
 
 @app.route("/find", methods=["GET"])
 def find():
-    return render_template("find.html")
+    #return render_template("find.html")
     # TODO
-    # return render_template("find.html", logo_location=logo_location)
+    food = db.recipe.find_one({"title": "김치볶음밥"})
+    logo_location = "static/logo.png"
+    return render_template("find.html", logo_location=logo_location)
 
 
 @app.route("/signin", methods=["GET"])
 def signin():
-    return render_template("signin.html")
+    #return render_template("signin.html")
     # TODO
-    # return render_template("signin.html", logo_location=logo_location)
+    food = db.recipe.find_one({"title": "김치볶음밥"})
+    logo_location = "static/logo.png"
+    return render_template("signin.html", logo_location=logo_location)
 
 
 if __name__ == "__main__":
